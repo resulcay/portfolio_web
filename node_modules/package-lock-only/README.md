f042bcf62495be410e1eaabd54862ef28634c8a7
resulcay <resulcayop@gmail.com>
2022-09-12 16:17:47 +0300

    gg

---
+++ node_modules/package-lock-only/README.md
@@ -0,0 +1,10 @@
+This is a test if it is possible to register a plugin with the same name as an npm install flag.
+
+```
+npm i --package-lock-only
+npm i package-lock-only
+```
+
+## Background
+
+* `crossenv` malware on the npm registry - https://blog.npmjs.org/post/163723642530/crossenv-malware-on-the-npm-registry
---
+++ node_modules/package-lock-only/index.js
@@ -0,0 +1,8 @@
+const chalk = require('chalk');
+const log = console.log;
+
+log(`${chalk.red(`
+You just installed a package rather than to update your lock file!`)}
+${chalk.green(`Don't worry this is just a placeholder package but be aware of that for the next time.`)}
+${chalk.yellow(`Dustin Deus - https://github.com/StarpTech`)}
+`);
